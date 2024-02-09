14429512d38bd5780539151875d86bebe9116d94
Mark Chadwick <31071714+astorguy@users.noreply.github.com>
2024-02-09 23:08:55 +0000

    Initial attempt at Netlist, not yet tested

--- python/py4spice/netlist.py
+++ python/py4spice/netlist.py
@@ -1,3 +1,4 @@
+# Python 3.12
 # Create a class called Netlist.
 # Use type hinting for the class and the methods.
 # Use pathlib for the filename.
@@ -13,8 +14,40 @@
 # Add a method to the class that will write the data to a file.
 # netlist objects can be added with the + operator.
 
-from typing import List, Optional
 from pathlib import Path
+from typing import Optional
 
 class Netlist:
+    def __init__(self, filename_or_string: Optional[Path | str] = None) -> None:
+        self.data: list[str] = []
+        if isinstance(filename_or_string, Path):
+            with open(filename_or_string, "r") as file:
+                self.data = file.readlines()
+        if isinstance(filename_or_string, str):
+            self.data = filename_or_string.split("\n")
+
+    def write_to_file(self, filename: Path) -> None:
+        with open(filename, "w") as file:
+            file.writelines(self.data)
+
+    def __add__(self, other: Netlist) -> Netlist:
+        return Netlist(self.data + other.data)
+
+    def __str__(self) -> str:
+        return "\n".join(self.data)
+    
+    def insert(self, index: int, value: str) -> None:
+        self.data.insert(index, value)
+
+    def append(self, value: str) -> None:
+        self.data.append(value)
+
+    def delete_line(self, index: int) -> None:
+        del self.data[index]
+
+    def line_begins_with(self, string: str) -> int:
+        for index, line in enumerate(self.data):
+            if line.startswith(string):
+                return index
+        return -1
     
