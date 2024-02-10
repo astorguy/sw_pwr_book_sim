856fbf1c5199b22bf87f8545d40f255b6f60aea4
Mark Chadwick <31071714+astorguy@users.noreply.github.com>
2024-02-10 04:25:58 +0000

    Add doc descriptions to each method

--- python/py4spice/netlist.py
+++ python/py4spice/netlist.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 
 class Netlist:
+    """Manipulates SPICE netlists"""
     def __init__(self, filename_or_string: Optional[Path | str] = None) -> None:
         self.data: list[str] = []
         if isinstance(filename_or_string, Path):
@@ -15,10 +16,12 @@ class Netlist:
         return "\n".join(self.data)
 
     def write_to_file(self, filename: Path) -> None:
+        """"Write netlist object data to a file"""
         with open(filename, "w") as file:
             file.write("\n".join(self.data))
 
     def __add__(self, other: "Netlist") -> "Netlist":
+        """Concatenate netlists with + operator"""
         combined_data = self.data + other.data
         return Netlist("\n".join(combined_data))
 
@@ -26,15 +29,18 @@ class Netlist:
         del self.data[index]
 
     def line_starts_with(self, string: str) -> int:
+        """returns index of first line that starts with string"""
         for i, line in enumerate(self.data):
             if line.startswith(string):
                 return i
         return -1
 
     def del_line_starts_with(self, string: str) -> None:
+        """deletes first line that starts with string"""
         index = self.line_starts_with(string)
         if index != -1:
             del self.data[index]
 
     def insert_line(self, index: int, line: str) -> None:
+        """inserts string line at index in data list"""
         self.data.insert(index, line)
