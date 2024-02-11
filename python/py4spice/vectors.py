2b216e66e613f3407328d997044f5b8f5000ebd3
Mark Chadwick <31071714+astorguy@users.noreply.github.com>
2024-02-11 01:20:54 +0000

    fixed some nagging issues

--- python/py4spice/vectors.py
+++ python/py4spice/vectors.py
@@ -5,8 +5,11 @@ class Vectors:
     def __init__(self, data: str) -> None:
         self.data = list(set(data.split()))
 
-    def list_out(self) -> List[str]:
-        return [str(item) for item in self.data]
+    def __str__(self) -> str:
+        return " ".join(self.data)
+
+    def list_out(self) -> list[str]:
+        return self.data
 
     def __add__(self, other: "Vectors") -> "Vectors":
         combined = self.data + other.data
