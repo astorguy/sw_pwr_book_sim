c5229c10101392e3e129dad23f8ca8ee909163d4
Mark Chadwick <31071714+astorguy@users.noreply.github.com>
2024-03-04 22:25:12 +0000

    small cleanup

--- python/sec_1_04_04_lin_reg.py
+++ python/sec_1_04_04_lin_reg.py
@@ -1,7 +1,16 @@
-# hello
+"""Example from Power Supply book: Section 1.04.04 Linear Regulator"""
+
+# region imports
+from pathlib import Path
+# endregion
+
+# region read toml
+# endregion
+
 
 def main() -> None:
     print("hello")
+
 
 if __name__ == "__main__":
     main()
