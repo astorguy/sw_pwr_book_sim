b86b41defaf6c57477b588e72c9f4342bac60fec
Mark Chadwick <31071714+astorguy@users.noreply.github.com>
2024-03-08 20:32:38 +0000

    set up config file decoding

--- python/sec_1_04_04_lin_reg.py
+++ python/sec_1_04_04_lin_reg.py
@@ -9,43 +9,60 @@ import py4spice as spi
 
 # endregion
 
-# region global variables
-# location of config.toml file and project name table name
-TOML_FILENAME: str = "/workspaces/sw_pwr_book_sim/python/config.toml"
-PROJ_NAME: str = "sec_1_04_04"
 
+# region important constants for reading config file
 
+# This constant is changed for each project. It is only referenced in the
+# config_file_decoding dictionary just below. It is not a global variable.
+MY_PROJECT: str = "sec_1_04_04"  # name matches the project section in the config file
+
+# These are keys for the config_file_decoding. They are referenced in other parts
+# of the code. They are the only true global variables in the code.
+CONFIG_NAME: str = "config_name"
+GLOBAL_SECTION: str = "global_section"
+NGSPICE_EXE_KEY: str = "ngspice_exe_key"
+NETLISTS_DIR_KEY: str = "netlists_dir_key"
+RESULTS_DIR_KEY: str = "results_dir_key"
+PROJ_PATH_KEY: str = "proj_path_key"
+PROJ_SECTION: str = "proj_section"
+
+# Dictionary for decoding the config file. Do not change values unless changing the
+# section or key names in the config file.
+config_file_decoding: dict[str, str] = {
+    CONFIG_NAME: "/workspaces/sw_pwr_book_sim/python/config.toml",
+    GLOBAL_SECTION: "global",
+    NGSPICE_EXE_KEY: "ngspice_exe_str",
+    NETLISTS_DIR_KEY: "netlists_dir_str",
+    RESULTS_DIR_KEY: "results_dir_str",
+    PROJ_PATH_KEY: "proj_path_str",
+    PROJ_SECTION: MY_PROJECT,
+}
 # endregion
 
 
 # region initialize
-def load_config(file_to_read: Path = Path("./config.toml")) -> dict[str, Any]:
-    """Read a config file in toml format and return a dictionary with the data.
-    If Path to file not given, read ./config.toml.
-    """
-    with open(file_to_read, "rb") as file:
-        toml_data: dict[str, Any] = tomllib.load(file)
-    return toml_data
+# def define_paths(my_config: dict[str, Any]) -> dict[str, Path]:
+#     """Define all the paths needed for the project
+#     based on the project path"""
 
+#     # Create dictionary of paths, starting with ngspice_exe a
+#     paths: dict[str, Path] = {"ngspice_exe": Path(my_config["GLOBAL"][NGSPICE_EXE_KEY])}
 
-def define_paths(ngspice_exe: Path, proj_path: Path) -> dict[str, Path]:
-    """Define all the paths needed for the project
-    based on the project path"""
+#     # Create project path and add to dictionary
+#     proj_path: Path = Path(my_config[PROJ_SECTION][PROJ_PATH_KEY])
+#     paths["proj_path"] = proj_path  # Add to dictionary
 
-    # Create dictionary of paths, starting with ngspice_exe and proj_path
-    paths: dict[str, Path] = {"ngspice_exe": ngspice_exe, "proj_path": proj_path}
+#     # Create sim_results directory if doesn't exist
+#     results_path: Path = proj_path / "sim_results"
+#     results_path.mkdir(parents=True, exist_ok=True)
+#     paths["results_path"] = results_path  # Add to dictionary
 
-    # Create sim_results directory if doesn't exist
-    results_path: Path = proj_path / "sim_results"
-    results_path.mkdir(parents=True, exist_ok=True)
-    paths["results_path"] = results_path  # Add to dictionary
+#     # Create netlists directory and files if they don't exist
+#     netlists_path: Path = proj_path / "netlists"
+#     netlists_path.mkdir(parents=True, exist_ok=True)  # Create the directory
+#     paths["netlists_path"] = netlists_path  # Add to dictionary
 
-    # Create netlists directory and files if they don't exist
-    netlists_path: Path = proj_path / "netlists"
-    netlists_path.mkdir(parents=True, exist_ok=True)  # Create the directory
-    paths["netlists_path"] = netlists_path  # Add to dictionary
-
-    return paths
+#     return paths
 
 
 def define_vector_sets() -> list[spi.Vectors]:
@@ -59,34 +76,24 @@ def define_vector_sets() -> list[spi.Vectors]:
     return [vec_all, vec_in_out]
 
 
-def initialize(config_filename: str, project: str) -> None:
-    """Initialize items that won't change during simulation and post-simulation
-    analysis
+def initialize(config_decoding: dict[str, str]) -> None:
+    """stuff"""
+    # read config file and create CONFIG dictionary
+    config_name: Path = Path(config_decoding[CONFIG_NAME])
+    with open(config_name, "rb") as file:
+        my_config: dict[str, Any] = tomllib.load(file)
 
-    Args:
-        config_filename (str): full path to configuration file
-        project (str): project name; determines table to read in config file
-
-    Returns:
-        write something here
-    """
-
-    # Read configuration file and set paths to Ngspice and project
-    config: dict[str, Any] = load_config(Path(config_filename))
-    ngspice_exe: Path = Path(config["global"]["ngspice_exe_str"])
-    proj_path: Path = Path(config[project]["proj_path_str"])
-
-    # create dictionary of paths including ngspice_exe and proj_path
-    my_paths: dict[str, Path] = define_paths(ngspice_exe, proj_path)
+    # create dictionary of paths
+    # my_paths: dict[str, Path] = define_paths(my_config, config_decoding)
+    # print(my_paths)
 
     # list_of_vectors: list[spi.Vectors] = define_vector_sets()
 
 
 # endregion
 
+
 # region simulate
-
-
 def simulate(ngspice_exe: Path, netlist: Path) -> str:
 
     # prepare simulate object, print out command, and simulate
@@ -100,7 +107,7 @@ def simulate(ngspice_exe: Path, netlist: Path) -> str:
 
 
 def main() -> None:
-    initialize(TOML_FILENAME, PROJ_NAME)
+    initialize(config_file_decoding)
     # finished: str = simulate(ngspice_exe, proj_path / "netlists/top1.cir")
     # print(finished)
 
