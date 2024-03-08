cfb9cfd1d5aa16e70e94c2c2ad9833624e4cd383
Mark Chadwick <31071714+astorguy@users.noreply.github.com>
2024-03-08 13:58:56 +0000

    create dict of paths

--- python/sec_1_04_04_lin_reg.py
+++ python/sec_1_04_04_lin_reg.py
@@ -3,7 +3,7 @@
 # region imports
 import tomllib
 from pathlib import Path
-from typing import Any, Tuple
+from typing import Any
 
 import py4spice as spi
 
@@ -28,15 +28,47 @@ def load_config(file_to_read: Path = Path("./config.toml")) -> dict[str, Any]:
     return toml_data
 
 
-def initialize(config_filename: str, project: str) -> Tuple[Path, Path]:
-    """Initialize to get ready for a simulation
+def define_paths(ngspice_exe: Path, proj_path: Path) -> dict[str, Path]:
+    """Define all the paths needed for the project
+    based on the project path"""
+
+    # Create dictionary of paths, starting with ngspice_exe and proj_path
+    paths: dict[str, Path] = {"ngspice_exe": ngspice_exe, "proj_path": proj_path}
+
+    # Create sim_results directory if doesn't exist
+    results_path: Path = proj_path / "sim_results"
+    results_path.mkdir(parents=True, exist_ok=True)
+    paths["results_path"] = results_path  # Add to dictionary
+
+    # Create netlists directory and files if they don't exist
+    netlists_path: Path = proj_path / "netlists"
+    netlists_path.mkdir(parents=True, exist_ok=True)  # Create the directory
+    paths["netlists_path"] = netlists_path  # Add to dictionary
+
+    return paths
+
+
+def define_vector_sets() -> list[spi.Vectors]:
+    """Define all the vector sets for simulation and post-simulation analysis
+
+    Returns:
+        list[spi.Vectors]: list of all the vector sets defined here
+    """
+    vec_all = spi.Vectors("all")
+    vec_in_out = spi.Vectors("in out")
+    return [vec_all, vec_in_out]
+
+
+def initialize(config_filename: str, project: str) -> None:
+    """Initialize items that won't change during simulation and post-simulation
+    analysis
 
     Args:
-        config_filename (str): full path with configuration file
+        config_filename (str): full path to configuration file
         project (str): project name; determines table to read in config file
 
     Returns:
-        Tuple[Path, Path]: returns info needed for subsequent steps
+        write something here
     """
 
     # Read configuration file and set paths to Ngspice and project
@@ -44,7 +76,10 @@ def initialize(config_filename: str, project: str) -> Tuple[Path, Path]:
     ngspice_exe: Path = Path(config["global"]["ngspice_exe_str"])
     proj_path: Path = Path(config[project]["proj_path_str"])
 
-    return (ngspice_exe, proj_path)
+    # create dictionary of paths including ngspice_exe and proj_path
+    my_paths: dict[str, Path] = define_paths(ngspice_exe, proj_path)
+
+    # list_of_vectors: list[spi.Vectors] = define_vector_sets()
 
 
 # endregion
@@ -65,9 +100,9 @@ def simulate(ngspice_exe: Path, netlist: Path) -> str:
 
 
 def main() -> None:
-    (ngspice_exe, proj_path) = initialize(TOML_FILENAME, PROJ_NAME)
-    finished: str = simulate(ngspice_exe, proj_path / "netlists/top1.cir")
-    print(finished)
+    initialize(TOML_FILENAME, PROJ_NAME)
+    # finished: str = simulate(ngspice_exe, proj_path / "netlists/top1.cir")
+    # print(finished)
 
 
 if __name__ == "__main__":
